13f3617b356a7bf437b6c9039a634b0b3cb5695e
Mehdi <mahdi644.emami@gmail.com>
2023-12-31 23:05:59 +0100

    Testing JS

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Find all div elements in the document
+const divs = document.querySelectorAll('div');
+
+// Loop through each div
+divs.forEach(function(div) {
+    // Check if the div contains the text 'Subscriptions'
+    if (div.textContent === 'Subscriptions') {
+        // Replace the content of the div with 'NotSubscription'
+        div.textContent = 'NotSubscription';
+    }
+});
+
+
 console.log("Hello world");
